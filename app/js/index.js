9cb3c9d9e5365f44d1a31d8000e86b559b425e38
E-Kirill <ezhov.kirill98@gmail.com>
2017-03-14 11:59:03 +0200

    scructure

---
+++ app/js/index.js
@@ -0,0 +1,3 @@
+function addTimeValue (e) {
+document.getElementById(e.target.id).innerHTML+= "<input type='number' name='' value='' id='numb' max='23' min='0'>:<input type='number' name='' value='' id='numb' max='23' min='0'><br>";
+}
